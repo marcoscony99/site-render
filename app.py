beeca4015fd32bb9973b48602d7e3f35cf7cc1ae
Marcos Cony <marcoscony@gmail.com>
2024-03-22 20:32:51 -0300

    testando 22 marco

--- app.py
+++ app.py
@@ -6,3 +6,10 @@ app = Flask(__name__)
 def index():
   return "Olá, <b>tudo bem</b>?"
       
+@app.route('/teste')
+def teste():
+  return "Essa página é um <b>teste do novo comando</b>."
+
+@app.route('/nome')
+def nome():
+    return 'Digite seu nome: <form method="post"><input type="text" name="nome"><input type="submit" value="Enviar"></form>'
